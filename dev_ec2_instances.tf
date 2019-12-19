94addc58a6f281e873ec7437eb3902d382eefd3b
Joe Mathai <joe.mathai@airbnb.com>
2019-12-19 13:46:56 -0800

    add second ec2 instance

--- dev_ec2_instances.tf
+++ dev_ec2_instances.tf
@@ -5,8 +5,10 @@ variable "region" {
   default = "us-west-2"
 }
 resource "aws_instance" "dev_ec2_instances" {
-  count = 2
   instance_type = "t2.micro"
   key_name = "jm430-aws-key"
-
+}
+resource "aws_instance" "dev_ec2_instance_2" {
+  instance_type = "t2.micro"
+  key_name = "jm430-aws-key"
 }
